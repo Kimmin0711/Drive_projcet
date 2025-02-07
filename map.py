f7f4bbe7ea8ae14fe1113dac4517ff6db4aa37f5
Kimmin0711 <easy000711@gmail.com>
2025-02-07 19:19:06 +0900

    Main Data

--- map.py
+++ map.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By  # By사용
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
 import time
 
 # 옵션 설정
